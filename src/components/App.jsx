3e70366f1b680c27d81ae665e2b2e43b68513ae0
Karol Kotański <karolfm12@onet.eu>
2024-04-08 13:22:54 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // let searchQuery;
 // let perPageItems;
@@ -12,13 +14,33 @@ class App extends Component {
     safesearch: true,
     per_page: 10,
     page: 1,
+    imageData: [],
   };
+
+  componentDidMount() {
+    // Instantiate the lightbox
+    this.lightbox = new simpleLightbox('.gallery a', {
+      captions: true,
+      captionSelector: 'img',
+      captionType: 'attr',
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // Refresh the lightbox when imageData changes
+    if (prevState.imageData !== this.state.imageData) {
+      this.lightbox.refresh();
+    }
+  }
+
   handleOnChange = e => {
     this.setState({
       q: e.target.value,
     });
-    console.log(this.state.q);
   };
+
   handleSubmit = e => {
     e.preventDefault();
     // const URL = `https://pixabay.com/api/?${this.state.q}`;
@@ -33,7 +55,9 @@ class App extends Component {
 
     fetch(URL)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => {
+        this.setState({ imageData: data.hits }); // Store fetched data in state
+      })
       .catch(error => {
         console.log(
           'Sorry, there are no images matching your search query. Please try again.'
@@ -41,7 +65,55 @@ class App extends Component {
       });
   };
 
+  // loadMoreImages = e => {
+  //   e.preventDefault();
+  //   this.setState(prevState => ({
+  //     per_page: prevState.per_page + 10,
+  //   }));
+  // };
+
+  // loadMoreImages = e => {
+  //   e.preventDefault();
+  //   this.setState(
+  //     prevState => ({
+  //       per_page: prevState.per_page + 10,
+  //       page: Math.floor((prevState.per_page + 10) / 10) + 1, // Increment page number
+  //     }),
+  //     () => {
+  //       this.handleSubmit(e); // Trigger search with updated parameters
+  //     }
+  //   );
+  // };
+
+  loadMoreImages = e => {
+    e.preventDefault();
+    const { key, q, image_type, orientation, safesearch, per_page, page } =
+      this.state;
+
+    const nextPage = page + 1; // Calculate the next page
+
+    const URL = `https://pixabay.com/api/?key=${key}&q=${encodeURIComponent(
+      q
+    )}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}&per_page=${per_page}&page=${nextPage}`;
+
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        this.setState(prevState => ({
+          imageData: [...prevState.imageData, ...data.hits],
+          page: nextPage,
+        }));
+      })
+      .catch(error => {
+        console.log(
+          'Sorry, there was an error while loading more images. Please try again.'
+        );
+      });
+  };
+
   render() {
+    const { imageData } = this.state;
+
     return (
       <>
         <header className="searchbar">
@@ -60,13 +132,26 @@ class App extends Component {
             />
           </form>
         </header>
-        <ul className="gallery">
-          {data.hits.map((value, index) => {
-            <li key={index} className="gallery-item">
-              <img src={value.largeImageURL} alt="" />
-            </li>;
-          })}
-        </ul>
+        {imageData.length > 0 ? (
+          <>
+            <ul className="gallery">
+              {imageData.map((value, index) => {
+                return (
+                  <li key={index} className="gallery-item">
+                    <a href={value.largeImageURL}>
+                      <img src={value.largeImageURL} alt="" />
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+            <button className="loadMore" onClick={this.loadMoreImages}>
+              Load More images
+            </button>
+          </>
+        ) : (
+          console.log('no image')
+        )}
       </>
     );
   }
